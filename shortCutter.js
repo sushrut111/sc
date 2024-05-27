5693f3729ecc023ed3f85cca434ecf73644e9568
Sushrut Kasture <kasturesushrut@gmail.com>
2024-05-27 15:34:44 +0530

    onload hook

--- shortCutter.js
+++ shortCutter.js
@@ -1,18 +1,22 @@
-var urlPath = window.location.hash;
-// get the leaf path 
-var shortUrlToBeUsed = urlPath
 
-var baseUrl = "https://github.com/sushrut111"
+document.onload(() => {
+    var urlPath = window.location.hash;
+    // get the leaf path 
+    var shortUrlToBeUsed = urlPath
+    
+    var baseUrl = "https://github.com/sushrut111"
+    
+    console.log(shortUrlToBeUsed)
+    
+    switch(shortUrlToBeUsed){
+        case "cowin":
+            window.location.assign(`${baseUrl}/cowin-automation-extn`);
+        case "cvq":
+            window.location.assign(`https://sushrut111.github.io/corona-virus-questionnaire/`);
+        case "autoresp":
+            window.location.assign(`${baseUrl}/requests-modifier`);
+        default:
+            document.body = "NO SHORTCUT FOUND";
+    }
+})
 
-console.log(shortUrlToBeUsed)
-
-switch(shortUrlToBeUsed){
-    case "cowin":
-        window.location.assign(`${baseUrl}/cowin-automation-extn`);
-    case "cvq":
-        window.location.assign(`https://sushrut111.github.io/corona-virus-questionnaire/`);
-    case "autoresp":
-        window.location.assign(`${baseUrl}/requests-modifier`);
-    default:
-        document.body = "NO SHORTCUT FOUND";
-}
